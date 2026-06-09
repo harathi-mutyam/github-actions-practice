bb1c09d5308916ebebccbec5f00ffd4537cfe28d
harathi-mutyam <ehmutyam@gmail.com>
2026-06-09 18:42:46 +0200

    test practice

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 
 secret = os.getenv("API_KEY")
+secret1 = os.getenv("test_secret")
 
 print("Secret value is:", secret)
+print("Secret value is:", secret1)
 print("Learning GitHub Actions")
